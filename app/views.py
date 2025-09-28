469a709380761653a89aa736a2b43fb2a07a4c00
Karthik <karthiky742@gmail.com>
2025-09-28 22:12:18 +0530

    Performed logics &  operations

--- app/views.py
+++ app/views.py
@@ -1,18 +1,62 @@
 from django.shortcuts import render,redirect
-from .forms import RegisterForm
+from .models import Account,Income,Expense
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
-
-def register(request):
-    form = RegisterForm()
-    if request.method=="POST":
-        form=RegisterForm(request.POST)
-        if form.is_valid():
-            print("successfull")
-            # form.save()
+    data=Account.objects.first()
     context={
-        'form':form
+        'data':data
     }
-    return render(request,'register.html',context)
+    return render(request,'index.html',context)
+
+def chart(request):
+    return render(request,'chart.html')
+
+def budget(request):
+    return render(request,'budget.html')
+
+
+def transaction(request):
+    data=Account.objects.first()
+    data1=None
+    data2=None
+    if request.method=="POST":
+        amt=request.POST.get('amt')
+        exp=request.POST.get('exp')
+        inc=request.POST.get('inc')
+        date=request.POST.get('date')
+        desc=request.POST.get('desc')
+        if data and amt:
+            amt=int(amt)
+
+            if inc:
+                data.cash+=amt
+                data.save()
+                Income.objects.create(
+                    income=inc,date=date,note=desc,amount=amt
+                )
+                data1.save()
+            if exp:
+                data.cash-=amt
+                data.save()
+                Expense.objects.create(
+                    expense=exp,date=date,note=desc,amount=amt
+                )
+                data2.save()
+    data1=Income.objects.all()
+    data2=Expense.objects.all()
+    context={
+        'data':data,
+        'data1':data1,
+        'data2':data2,
+    }
+    return render(request,'transaction.html',context)
+
+def delete_inc(request,d1):
+    data=Income.objects.get(id=d1)
+    data.delete()
+    return redirect('transaction')
+def delete_exp(request,d2):
+    data=Expense.objects.get(id=d2)
+    data.delete()
+    return redirect('transaction')
