4fd30e0a69ee7d4bbf260fd466125b84a4242ec6
Karthik <karthiky742@gmail.com>
2025-10-08 16:08:48 +0530

    added some paths

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index,name='index'),
-    path('register/',views.register,name='register')
+    path('',views.index,name='dashboard'),
+    path('chart/',views.chart,name='chart'),
+    path('budget/',views.budget,name="budget"),
+    path('transaction/',views.transaction,name="transaction"),
+    path('delete/income/<int:d1>',views.delete_inc,name="delete1"),
+    path('delete/expense/<int:d2>',views.delete_exp,name='delete2'),
 ]
