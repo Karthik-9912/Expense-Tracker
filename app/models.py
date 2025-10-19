7fc36a74baf6d36641e47eb789b500adf194e09a
Karthik <karthiky742@gmail.com>
2025-10-19 10:52:58 +0530

    Models updated

--- app/models.py
+++ app/models.py
@@ -1,19 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Account(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
     cash=models.DecimalField(default=0,max_digits=9,decimal_places=2)
 
-    def __str__(self):
-        return str(self.cash)
+    # def __str__(self):
+    #     return str(self.cash)
     
 class Expense(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     expense_type=models.CharField(max_length=32)
     date=models.DateField()
     note=models.TextField(max_length=100)
     amount=models.DecimalField(max_digits=9,decimal_places=2)
 
 class Income(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     income_type=models.CharField(max_length=32)
     date=models.DateField()
     note=models.TextField(max_length=100)
