224be196ab1daa4d1f0b413b1778a2c56b1af3a3
Karthik <karthiky742@gmail.com>
2025-10-22 13:15:02 +0530

    transaction beahviours

--- app/static/app/js/transaction.js
+++ app/static/app/js/transaction.js
@@ -1,7 +1,15 @@
 let btn=document.querySelector("#transactionBtn");
 let div=document.querySelector(".spent-sec")
 
-btn.addEventListener("click",()=>{
+btn.addEventListener("click",(event)=>{
+    event.stopPropagation();
     div.classList.toggle("visible")
     
+});
+
+// 
+window.addEventListener("click",(event)=>{
+    if(!div.contains(event.target) && !btn.contains(event.target)){
+        div.classList.remove("visible")
+    }
 });
